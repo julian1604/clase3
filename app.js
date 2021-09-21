8662829ddb26f2f055cc0fa21952925d2ada3621
julian1604 <71854198+julian1604@users.noreply.github.com>
2021-09-21 08:55:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World. Este es un ejemplo para la clase de DevOps. This page is running Node.js version: ");
+  response.write("Hello World. Este es un ejemplo de Cont/Deployment. This page is running Node.js version: ");
   response.write(process.version);
   response.end();
 }).listen(8888);
